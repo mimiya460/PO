a4460f5d63087fc2f7b4b40803ed6e471e92b365
mimi <kafandi@163.com>
2019-08-15 10:44:22 +0800

    3g

--- scripts/test_network抽取元素的特征.py
+++ scripts/test_network抽取元素的特征.py
@@ -1,4 +1,4 @@
-# import os,sys
+﻿# import os,sys
 # sys.path.append(os.getcwd())
 # from base.base_driver import init_driver
 # from network_page分离测试脚本 import DisplayPage
@@ -10,7 +10,10 @@
 #         self.network_page.anquan_click()
 #         self.network_page.pingmusd_click()
 #         self.network_page.huadong_click()
-#
+# def test_mobile_network_3g(self):
+#         self.network_page.anquan_click()
+#         self.network_page.pingmusd_click()
+#         self.network_page.huadong_click()
 #         # self.driver.find_element_by_xpath("//*[contains(@text,'安全')]").click()
 #         # self.driver.find_element_by_xpath("//*[contains(@text,'屏幕锁定')]").click()
 #         # self.driver.find_element_by_xpath("//*[contains(@text,'滑动')]").click()
